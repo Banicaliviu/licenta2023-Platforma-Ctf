9c908bdd8953f836265f068ca12eeb3b7a327593
LiviuBanica <Banicaliviu>
2023-04-07 09:43:48 -0700

    Added Login, updated base.html, updated login and register, organised app :auth and main blueprint, added utils.py for utility functions

--- 06042023_licenta/flaskapp/ctfplatform/__init__.py
+++ 06042023_licenta/flaskapp/ctfplatform/__init__.py
@@ -3,4 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = "CTFPlatform"
 
-from ctfplatform import routes
+from ctfplatform.auth import auth_bp
+from ctfplatform.main import main_bp
+
+app.register_blueprint(auth_bp)
+app.register_blueprint(main_bp)
